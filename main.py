940835adf8021bdcf821a3327d81f8616c169e7c
KimNamRyeol <51590233+KimNamRyeol@users.noreply.github.com>
2025-05-15 13:21:41 +0900

    Update main.py
    잘못된 수정 다시 돌려놓음

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from domain import game_router, user_router
 from database import Base, engine
 
 app = FastAPI()
-
-app.mount(
-     "/media",
-     StaticFiles(directory="uploaded_images"),
-     name="media"
- )
 
 Base.metadata.create_all(bind=engine)
 
